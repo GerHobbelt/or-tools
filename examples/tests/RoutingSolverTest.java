3fe1546872e4a787522547e519d5d00f904865cf
Corentin Le Molgat <corentinl@google.com>
2021-08-31 11:58:46 +0200

    WIP: sync java tests

--- examples/tests/RoutingSolverTest.java
+++ examples/tests/RoutingSolverTest.java
@@ -13,6 +13,9 @@
 package com.google.ortools;
 
 import static java.lang.Math.abs;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.ortools.Loader;
 import com.google.ortools.constraintsolver.Assignment;
@@ -22,18 +25,124 @@ import com.google.ortools.constraintsolver.RoutingModel;
 import com.google.ortools.constraintsolver.RoutingSearchParameters;
 import com.google.ortools.constraintsolver.main;
 import com.google.ortools.constraintsolver.IntBoolPair;
+import java.util.ArrayList;
+import java.util.function.LongBinaryOperator;
+import java.util.function.LongUnaryOperator;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /** Tests the Routing java interface. */
-public class RoutingSolverTest {
+public final class RoutingSolverTest {
   private static final Logger logger = Logger.getLogger(RoutingSolverTest.class.getName());
+  private ArrayList<Integer[]> coordinates;
+
+  @BeforeEach
+  public void setUp() {
+    Loader.loadNativeLibraries();
+    coordinates = new ArrayList<>();
+    coordinates.add(new Integer[] {0, 0});
+    coordinates.add(new Integer[] {-1, 0});
+    coordinates.add(new Integer[] {-1, 2});
+    coordinates.add(new Integer[] {2, 1});
+    coordinates.add(new Integer[] {1, 0});
+  }
+
+  public LongBinaryOperator createManhattanCostCallback(RoutingIndexManager manager) {
+    return (long i, long j) -> {
+        final int firstIndex = manager.indexToNode(i);
+        final int secondIndex = manager.indexToNode(j);
+        final Integer[] firstCoordinate = coordinates.get(firstIndex);
+        final Integer[] secondCoordinate = coordinates.get(secondIndex);
+        return (long) Math.abs(firstCoordinate[0] - secondCoordinate[0])
+            + Math.abs(firstCoordinate[1] - secondCoordinate[1]);
+    };
+  }
+
+  public LongUnaryOperator createUnaryCostCallback(RoutingIndexManager manager) {
+    return (long fromIndex) -> {
+        final int fromNode = manager.indexToNode(fromIndex);
+        final Integer[] firstCoordinate = coordinates.get(fromNode);
+        return (long) Math.abs(firstCoordinate[0]) + Math.abs(firstCoordinate[1]);
+    };
+  }
+
+  public LongBinaryOperator createReturnOneCallback() {
+    return (long i, long j) -> 1;
+  }
+
+  @Test
+  public void testRoutingIndexManager() {
+    final RoutingIndexManager manager = new RoutingIndexManager(42, 3, 7);
+    assertNotNull(manager);
+    assertEquals(42, manager.getNumberOfNodes());
+    assertEquals(3, manager.getNumberOfVehicles());
+    assertEquals(42 + 3 * 2 - 1, manager.getNumberOfIndices());
+    for (int i = 0; i < manager.getNumberOfVehicles(); ++i) {
+      assertEquals(7, manager.indexToNode(manager.getStartIndex(i)));
+      assertEquals(7, manager.indexToNode(manager.getEndIndex(i)));
+    }
+  }
+
+  @Test
+  public void testRoutingIndexManagerMultiDepotSame() {
+    final RoutingIndexManager manager =
+        new RoutingIndexManager(42, 3, new int[] {7, 7, 7}, new int[] {7, 7, 7});
+    assertNotNull(manager);
+    assertEquals(42, manager.getNumberOfNodes());
+    assertEquals(3, manager.getNumberOfVehicles());
+    assertEquals(42 + 3 * 2 - 1, manager.getNumberOfIndices());
+    for (int i = 0; i < manager.getNumberOfVehicles(); ++i) {
+      assertEquals(7, manager.indexToNode(manager.getStartIndex(i)));
+      assertEquals(7, manager.indexToNode(manager.getEndIndex(i)));
+    }
+  }
+
+  @Test
+  public void testRoutingIndexManagerMultiDepotAllDifferent() {
+    final RoutingIndexManager manager =
+        new RoutingIndexManager(42, 3, new int[] {1, 2, 3}, new int[] {4, 5, 6});
+    assertNotNull(manager);
+    assertEquals(42, manager.getNumberOfNodes());
+    assertEquals(3, manager.getNumberOfVehicles());
+    assertEquals(42, manager.getNumberOfIndices());
+    for (int i = 0; i < manager.getNumberOfVehicles(); ++i) {
+      assertEquals(i + 1, manager.indexToNode(manager.getStartIndex(i)));
+      assertEquals(i + 4, manager.indexToNode(manager.getEndIndex(i)));
+    }
+  }
+
+  @Test
+  public void testRoutingModel() {
+    final RoutingIndexManager manager =
+        new RoutingIndexManager(42, 3, new int[] {1, 2, 3}, new int[] {4, 5, 6});
+    assertNotNull(manager);
+    final RoutingModel model = new RoutingModel(manager);
+    assertNotNull(model);
+    for (int i = 0; i < manager.getNumberOfVehicles(); ++i) {
+      assertEquals(i + 1, manager.indexToNode(model.start(i)));
+      assertEquals(i + 4, manager.indexToNode(model.end(i)));
+    }
+  }
+
+  @Test
+  public void testCostsAndSolve() {
+    final RoutingIndexManager manager = new RoutingIndexManager(coordinates.size(), 1, 0);
+    final RoutingModel model = new RoutingModel(manager);
+    assertEquals(5, model.nodes());
+    final LongBinaryOperator callback = createManhattanCostCallback(manager);
+    final int cost = model.registerTransitCallback(callback);
+    model.setArcCostEvaluatorOfAllVehicles(cost);
+    assertEquals(RoutingModel.ROUTING_NOT_SOLVED, model.status());
+    final Assignment solution = model.solve(null);
+    assertEquals(RoutingModel.ROUTING_SUCCESS, model.status());
+    assertEquals(10, solution.objectiveValue());
+  }
 
   @Test
   public void testRoutingTransitMatrix() {
-    Loader.loadNativeLibraries();
     logger.info("testRoutingTransitMatrix...");
     // Create Routing Index Manager
     RoutingIndexManager manager =
@@ -69,7 +178,6 @@ public class RoutingSolverTest {
   @ParameterizedTest
   @ValueSource(booleans = {false, true})
   public void testRoutingTransitCallback(boolean enableGC) {
-    Loader.loadNativeLibraries();
     logger.info("testRoutingTransitCallback (enable gc:" + enableGC + ")...");
     // Create Routing Index Manager
     RoutingIndexManager manager =
@@ -107,7 +215,6 @@ public class RoutingSolverTest {
 
   @Test
   public void testRoutingMatrixDimension() {
-    Loader.loadNativeLibraries();
     logger.info("testRoutingMatrixDimension...");
     // Create Routing Index Manager
     RoutingIndexManager manager =
@@ -146,7 +253,6 @@ public class RoutingSolverTest {
 
   @Test
   public void testRoutingUnaryTransitVector() {
-    Loader.loadNativeLibraries();
     logger.info("testRoutingUnaryTransitVector...");
     // Create Routing Index Manager
     RoutingIndexManager manager =
@@ -176,7 +282,6 @@ public class RoutingSolverTest {
   @ParameterizedTest
   @ValueSource(booleans = {false, true})
   public void testRoutingUnaryTransitCallback(boolean enableGC) {
-    Loader.loadNativeLibraries();
     logger.info("testRoutingUnaryTransitCallback (enable gc:" + enableGC + ")...");
     // Create Routing Index Manager
     RoutingIndexManager manager =
@@ -213,7 +318,6 @@ public class RoutingSolverTest {
 
   @Test
   public void testRoutingVectorDimension() {
-    Loader.loadNativeLibraries();
     logger.info("testRoutingVectorDimension...");
     // Create Routing Index Manager
     RoutingIndexManager manager =
